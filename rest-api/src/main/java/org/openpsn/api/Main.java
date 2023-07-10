507b0f8968641aa75752e1388d271d254232156d
David Kosub <dave@kosub.us>
2023-07-09 20:24:08 -0500

    chore(rest-api): update to jooby 3 stable

--- rest-api/src/main/java/org/openpsn/api/Main.java
+++ rest-api/src/main/java/org/openpsn/api/Main.java
@@ -4,6 +4,6 @@ import io.jooby.Jooby;
 
 public class Main {
     public static void main(String[] args) {
-        Jooby.runApp(args, ApiApplication.class);
+        Jooby.runApp(args, ApiApplication::new);
     }
 }
