b4125b89fef9ca517b8f749398120ecd52e7d7d3
David Kosub <dave@kosub.us>
2023-07-11 18:20:30 -0500

    chore(web-app): fix logout flow

--- web-app/src/routes/+layout.svelte
+++ web-app/src/routes/+layout.svelte
@@ -8,7 +8,11 @@
 <nav>
   <ul>
     {#if data?.user}
-      <li><a href="/logout">Log Out</a></li>
+      <li>
+        <form action="/logout" method="POST">
+          <button type="submit">Log Out</button>
+        </form>
+      </li>
     {:else}
       <li><a href="/register">Register</a></li>
       <li><a href="/login">Log In</a></li>
--- web-app/src/routes/logout/+page.server.ts
+++ web-app/src/routes/logout/+page.server.ts
@@ -1,7 +1,15 @@
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
-export const load: PageServerLoad = ({ cookies }) => {
-  cookies.delete('session');
+export const load: PageServerLoad = ({ cookies, locals }) => {
   throw redirect(303, '/');
+};
+
+export const actions: Actions = {
+  default: ({ cookies, locals }) => {
+    cookies.delete('session');
+    locals.user = null;
+
+    throw redirect(303, '/');
+  },
 };
